ed08c358318b0f2972be8a391ec495ca671f2597
mdtowfiq300 <mdtowfiq600@gmail.com>
2025-09-02 16:14:58 +0600

    Update common.js

--- accident_layout/common.js
+++ accident_layout/common.js
@@ -1,1 +1,16 @@
+document.getElementById('loginBtn').addEventListener('click', function () {
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
 
+    // Predefined login credentials
+    
+    const ohs = { phone: 'ccdc10x', password: 'ccdc10x', redirectUrl: 'ohs.html' };
+
+    // Validate login
+    if (phoneNumber === ohs.phone && password === ohs.password) {
+        window.location.href = accident.redirectUrl;
+    } else {
+        errorMessage.textContent = 'Invalid phone number or password';
+    }
+});
